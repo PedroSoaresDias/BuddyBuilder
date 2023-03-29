81de413b618a3e5b993ff2dd3b3ff97e1e4a36e9
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-03-29 13:26:39 -0300

    Criado página de Login e Cadastro

---
+++ Pages/Cadastro.js
@@ -0,0 +1,123 @@
+import { StatusBar, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, View, TextInput, Image, TouchableOpacity } from "react-native";
+
+const SignUpScreen = () => {
+    const [nickname, setNickname] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSignUp = () => {
+        // implementar lógica de validação do lado do cliente e armazenamento de dados
+    };
+}
+
+export default function Cadastro() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textNickname}>Apelido</Text>
+            <TextInput style={styles.nicknameInput} placeholder="Apelido" onChangeText={text => setNickname(text)} value={nickname} />
+
+            <Text style={styles.textEmail}>E-mail</Text>
+            <TextInput style={styles.emailInput} placeholder="E-mail" onChangeText={text => setEmail(text)} value={email} />
+
+            <Text style={styles.textPassword}>Senha</Text>
+            <TextInput placeholder="Senha" onChangeText={text => setPassword(text)} value={password} />
+
+            <Text style={styles.textConfirm}>Confirmar senha</Text>
+            <TextInput style={styles.confirmPasswordInput} placeholder="Confirmar senha" onChangeText={text => setConfirmPassWord(text)} value={confirmPassword} />
+
+            <TouchableOpacity style={styles.btnCadastro}><Text style={styles.textCadastro}>Concluir cadastro</Text></TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F5F5F5",
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    nicknameInput: {
+        width: 191,
+        height: 26,
+        top: 143,
+        left: 31,
+        borderRadius: 5,
+    },
+    emailInput: {
+        width: 191,
+        height: 26,
+        top: 213,
+        left: 31,
+        borderRadius: 5,
+    },
+    passwordInput: {
+        width: 191,
+        height: 26,
+        top: 283,
+        left: 31,
+        borderRadius: 5,
+    },
+    confirmPasswordInput: {
+        width: 191,
+        height: 36,
+        top: 353,
+        left: 31,
+        borderRadius: 5,
+    },
+    btnCadastro: {
+        width: 191,
+        height: 36,
+        top: 403,
+        left: 44,
+        borderRadius: 8
+    },
+    textNickname: {
+        fontSize: 14,
+        fontFamily: 'Inter',
+        fontWeight: 400,
+        width: 50,
+        height: 17,
+        top: 123,
+        left: 31,
+    },
+    textEmail: {
+        fontFamily: 'Inter',
+        fontSize: 14,
+        fontWeight: 400,
+        width: 42,
+        height: 17,
+        top: 193,
+        left: 31,
+    },
+    textPassword: {
+        fontFamily: 'Inter',
+        fontSize: 14,
+        fontWeight: 400,
+        width: 42,
+        height: 17,
+        top: 263,
+        left: 31,
+    },
+    textConfirm: {
+        fontFamily: 'Inter',
+        fontSize: 14,
+        fontWeight: 400,
+        width: 111,
+        height: 17,
+        top: 333,
+        left: 31,
+    },
+    textCadastro: {
+        width: 122,
+        height: 17,
+        top: 413,
+        left: 65,
+        fontFamily: 'Inter',
+        fontSize: 14,
+        fontWeight: 700,
+    }
+})
---
+++ Pages/Login.js
@@ -0,0 +1,87 @@
+import { StatusBar } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet,  View, TextInput, Text, TouchableOpacity} from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // implementar lógica de validação do lado do cliente e armazenamento de dados
+  };
+};
+
+export default function Login() {
+  return (
+    // <View style={styles.container}>
+
+    // </View>
+    <View>
+        <Text>E-mail</Text>
+        <TextInput style={styles.inputEmail} placeholder="E-mail" onChangeText={(text) => setEmail(text)} value={email} />
+
+        <Text>Senha</Text>
+        <TextInput style={styles.inputPassword} placeholder="Senha" onChangeText={(text) => setPassword(text)} value={password} />
+
+        <TouchableOpacity style={styles.btnLogin}><Text>Login</Text></TouchableOpacity>
+            
+        <TouchableOpacity style={styles.btnCadastro}><Text>Cadastrar-se</Text></TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        width: 253,
+        height: 300,
+        top: 0,
+        left: 0,
+        backgroundColor: "#B6D1D1",
+    },
+    inputEmail: {
+        width: 191,
+        height: 26,
+        top: 340,
+        left: 31,
+        borderRadius: 5,
+    },
+    inputPassword: {
+        width: 191,
+        height: 26,
+        top: 377,
+        left: 31,
+        borderRadius: 5
+    },
+    btnLogin: {
+        width: 129,
+        height: 38,
+        top: 438,
+        left: 62,
+        borderRadius: 8,
+    },
+    btnCadastro: {
+        width: 129,
+        height: 38,
+        top: 492,
+        left: 62,
+        borderRadius: 8,
+    },
+    textLogin: {
+        fontFamily: 'Inter',
+        height: 17,
+        top: 448,
+        left: 104,
+        fontSize: 14,
+        fontWeight: 700,
+    },
+    textCadastro: {
+        fontFamily: 'Inter',
+        fontSize: 14,
+        fontWeight: 700,
+        width: 91,
+        height: 17,
+        top: 502,
+        left: 81
+    }
+});
